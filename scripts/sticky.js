d1f6c608132472d8913a86fb4f8615629d6d72e2
cwyptid <elampert@kent.edu>
2024-01-08 12:14:09 -0500

    Committing extra styles to fix sticky nav and testimonial.

--- scripts/sticky.js
+++ scripts/sticky.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$(window).scroll(function () {
 		var scrollTop = $(window).scrollTop();
-		if (scrollTop > 0) {
+		if (scrollTop) {
 			$("body").addClass("header-fixed");
 		} else {
 			$("body").removeClass("header-fixed");
